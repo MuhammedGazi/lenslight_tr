b22152f79e693ce90ec70ce930c2306a5b3c2ddd
MuhammedGazi <muhammedgazicelenk@gmail.com>
2023-03-11 22:29:58 +0300

    lesson 02

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
 
-const app=express();
-const port=300;
+dotenv.config();
 
-app.get("/",(req,res)=>{
-    res.send('INDEX SAYFASI');
-});
-app.listen(port,()=>{
+//connection to the DB
+conn();
+
+const app = express();
+const port = process.env.PORT;
+
+//ejs template engine
+app.set("view engine", 'ejs');
+
+//static files midleware
+app.use(express.static("public"));
+app.use(express.json());
+
+// routes
+app.use("/", pageRoute);
+app.use("/photos",photoRoute);
+
+
+app.listen(port, () => {
     console.log(`application running on port: http://localhost:${port}`);
 });
